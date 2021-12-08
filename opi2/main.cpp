c5ab792cd2571997082f19f188344c0d1012943c
lilobanova <lilobanova10@gmail.com>
2021-12-08 14:54:01 +0300

    s_delete_key

--- opi2/main.cpp
+++ opi2/main.cpp
@@ -22,3 +22,17 @@ void s_push(comp **top, int D) {
 		*top = q;
 	}
 }
+
+void s_delete_key(comp **top, int N) {
+	comp *q = *top;
+	comp *prev = NULL;
+	while (q != NULL) {
+		if (q->Data == N) {
+			if (q == *top) {
+				*top = q->next;
+				free(q);
+				q->Data = NULL;  //или числа  "-2738568384" или других, в зависимости от компил€тора.
+				q->next = NULL;
+			}
+
+}
